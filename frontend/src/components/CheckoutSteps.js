27bddbe0563861e2f067eb181278b3c2e477a9ef
Anand Devarajan <ananddevarajan01@gmail.com>
2020-12-29 12:51:39 +0530

    order summary added

--- frontend/src/components/CheckoutSteps.js
+++ frontend/src/components/CheckoutSteps.js
@@ -8,7 +8,11 @@ const CheckoutSteps = ({ step1, step2, step3, step4 }) => {
       <Nav.Item>
         {step1 ? (
           <LinkContainer to='/login'>
-            <Nav.Link>Sign In</Nav.Link>
+            <Nav.Link>
+              <strong style={{ fontWeight: 'bold', color: 'green' }}>
+                Sign In
+              </strong>
+            </Nav.Link>
           </LinkContainer>
         ) : (
           <Nav.Link disabled>Sign In</Nav.Link>
@@ -17,7 +21,9 @@ const CheckoutSteps = ({ step1, step2, step3, step4 }) => {
       <Nav.Item>
         {step2 ? (
           <LinkContainer to='/shipping'>
-            <Nav.Link>Shipping</Nav.Link>
+            <Nav.Link> <strong style={{ fontWeight: 'bold', color: 'green' }}>
+                Shipping
+              </strong></Nav.Link>
           </LinkContainer>
         ) : (
           <Nav.Link disabled>Shipping</Nav.Link>
@@ -26,7 +32,9 @@ const CheckoutSteps = ({ step1, step2, step3, step4 }) => {
       <Nav.Item>
         {step3 ? (
           <LinkContainer to='/payment'>
-            <Nav.Link>Payment</Nav.Link>
+            <Nav.Link> <strong style={{ fontWeight: 'bold', color: 'green' }}>
+                Payment
+              </strong></Nav.Link>
           </LinkContainer>
         ) : (
           <Nav.Link disabled>Payment</Nav.Link>
@@ -35,7 +43,9 @@ const CheckoutSteps = ({ step1, step2, step3, step4 }) => {
       <Nav.Item>
         {step4 ? (
           <LinkContainer to='/placeorder'>
-            <Nav.Link>Place Order</Nav.Link>
+            <Nav.Link> <strong style={{ fontWeight: 'bold', color: 'green' }}>
+                Place Order
+              </strong></Nav.Link>
           </LinkContainer>
         ) : (
           <Nav.Link disabled>Place Order</Nav.Link>
