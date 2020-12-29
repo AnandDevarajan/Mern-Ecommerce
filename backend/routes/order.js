5ca41bcd45b27e26d35bb812645b64e6dfb67d85
Anand Devarajan <ananddevarajan01@gmail.com>
2020-12-29 19:33:35 +0530

    order route created

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { addOrderItems } = require('../controllers/order');
+const { addOrderItems, getOrderById } = require('../controllers/order');
 const { verifyAuth } = require('../middleware/auth');
 
 router.post('/', verifyAuth, addOrderItems);
+router.get('/:id', verifyAuth, getOrderById);
 
 module.exports = router;
