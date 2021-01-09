4519b0480dba56cf9d6fe48e3d9ab98135bc5dd9
Anand Devarajan <ananddevarajan01@gmail.com>
2021-01-09 10:01:19 +0530

    upload route added

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 require('dotenv').config();
+const path = require('path');
 const connectDB = require('./config/db');
 const productRoutes = require('./routes/product');
 const userRoutes = require('./routes/user');
 const orderRoutes = require('./routes/order');
+const uploadRoutes = require('./routes/upload');
 const cors = require('cors');
 const { notFound, errorHandler } = require('./middleware/errorMiddleware');
 const app = express();
@@ -18,11 +20,13 @@ app.get('/', (req, res) => {
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
+app.use('/api/upload', uploadRoutes);
 
 app.get('/api/config/paypal', (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID);
 });
 
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 app.use(notFound);
 app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
