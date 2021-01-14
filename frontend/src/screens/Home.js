5eee8212c04a274cc5dbcf0b9c443df7a4979989
Anand Devarajan <ananddevarajan01@gmail.com>
2021-01-14 09:41:04 +0530

    home screen pagination

--- frontend/src/screens/Home.js
+++ frontend/src/screens/Home.js
@@ -4,13 +4,14 @@ import Product from '../components/Product';
 import { useDispatch, useSelector } from 'react-redux';
 import { listProducts } from '../actions/productActions';
 import Loader from '../components/Loader';
+import Paginate from '../components/Paginate';
 
 const Home = ({ match }) => {
   const keyword = match.params.keyword;
   const pageNumber = match.params.pageNumber || 1;
   const dispatch = useDispatch();
   const productList = useSelector((state) => state.productList);
-  const { loading, error, products } = productList;
+  const { loading, error, products, page, pages } = productList;
 
   useEffect(() => {
     dispatch(listProducts(keyword, pageNumber));
@@ -32,6 +33,11 @@ const Home = ({ match }) => {
               </Col>
             ))}
           </Row>
+          <Paginate
+            pages={pages}
+            page={page}
+            keyword={keyword ? keyword : ''}
+          />
         </>
       )}
     </>
