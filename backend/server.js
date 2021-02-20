30d34f41bca526ba1f77c4d601fcfd7d8ff221aa
Anand Devarajan <ananddevarajan01@gmail.com>
2021-02-20 11:10:44 +0530

    production build

--- backend/server.js
+++ backend/server.js
@@ -17,9 +17,6 @@ if (process.env.NODE_ENV === 'development') {
 }
 app.use(cors());
 app.use(express.json());
-app.get('/', (req, res) => {
-  res.send('API is running');
-});
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
@@ -29,8 +26,22 @@ app.use('/api/upload', uploadRoutes);
 app.get('/api/config/paypal', (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID);
 });
+
 const dirname = path.resolve();
+
 app.use('/uploads', express.static(path.join(dirname, '/uploads')));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(dirname, '/frontend/build')));
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running');
+  });
+}
+
 app.use(notFound);
 app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
